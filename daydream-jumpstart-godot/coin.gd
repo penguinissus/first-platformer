2952e9cad0a1f4f7ec945843b12cccbd6a8cf76d
Jocelyn Lau <jocelyn.ty.lau@gmail.com>
2025-08-08 12:14:53 -0400

    coin script

--- daydream-jumpstart-godot/coin.gd
+++ daydream-jumpstart-godot/coin.gd
@@ -1,11 +1,8 @@
 extends Node2D
 
+@export var value: int = 1
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_area_2d_body_entered(body):
+	if body is Player:
+		GameController.coin_collected(value)
+		self.queue_free()
